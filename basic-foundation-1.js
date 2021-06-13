8393b17673540c93c55429eaccb3da2cba685573
Sevrinn <sevrinnsevrinn@gmail.com>
2021-06-12 22:35:32 -0700

    added sumOfOdd function

--- basic-foundation-1.js
+++ basic-foundation-1.js
@@ -2,13 +2,16 @@
  * 1. Get 1 to 255 
  * Write a function that returns an array with all the numbers from 1 to 255.    
  */
-function createArray() {
+function oneToTwoFiftyFive() {
     var arr = [];
     for (var i = 1; i < 256; i++) {
         arr.push(i);
     }
     return arr;
 }
+
+//uncomment line below to run function
+//oneToTwoFiftyFive();
 
 /**
  * 2. Get even 1000 
@@ -25,3 +28,22 @@ function sumOfEven() {
     }
     return sum;
 }
+//uncomment line below to run function
+//sumOfEven();
+
+/**
+ * Sum odd 5000 
+ * Write a function that returns the sum of all the odd numbers from 1 to 5000.
+ */
+
+function sumOfOdd() {
+    var sum = 0;
+    for (var i = 1; i < 5001; i++) {
+        if (i % 2 != 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+//uncomment line below to run function
+//sumOfOdd();
